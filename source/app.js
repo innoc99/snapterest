346b19aa85efb4f0a73077840d52ede122e2fc78
illy <innoc@daum.net>
2017-01-22 18:21:23 +0900

    react base commit

--- source/app.js
+++ source/app.js
@@ -1,1 +1,7 @@
+let React = require('react');
+let ReactDOM = require('react-dom');
+
+let reactElement = React.createElement('h1', {className:'header'}, 'This is react');
+ReactDOM.render(reactElement, document.getElementById('reactapplication'));
+
 console.log("test");
